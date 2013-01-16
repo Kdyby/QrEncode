4fb776c26ba9cb6b78eb86fc253fd3188f8e8aca
Filip Procházka <hosiplan@gmail.com>
2013-01-16 12:02:12 +0100

    copyright years

--- src/Kdyby/QrEncode/DI/Configuration.php
+++ src/Kdyby/QrEncode/DI/Configuration.php
@@ -3,21 +3,20 @@
 /**
  * This file is part of the Kdyby (http://www.kdyby.org)
  *
- * Copyright (c) 2008, 2012 Filip Procházka (filip@prochazka.su)
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
  *
  * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
  */
 
 namespace Kdyby\QrEncode\DI;
-
 use Kdyby;
-use Kdyby\Curl\Request;
 use Kdyby\Curl\CurlException;
-use Nette;
-use Nette\Http\Url;
-use Nette\Diagnostics\Debugger;
-use Kdyby\QrEncode\QrCode;
+use Kdyby\Curl\Request;
 use Kdyby\QrEncode\InvalidStateException;
+use Kdyby\QrEncode\QrCode;
+use Nette;
+use Nette\Diagnostics\Debugger;
+use Nette\Http\Url;
 
 
 
--- src/Kdyby/QrEncode/QrCodeResponse.php
+++ src/Kdyby/QrEncode/QrCodeResponse.php
@@ -3,17 +3,16 @@
 /**
  * This file is part of the Kdyby (http://www.kdyby.org)
  *
- * Copyright (c) 2008, 2012 Filip Procházka (filip@prochazka.su)
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
  *
  * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
  */
 
 namespace Kdyby\QrEncode;
-
 use Kdyby;
+use Kdyby\QrEncode\DI\Configuration;
 use Nette;
 use Nette\Diagnostics\Debugger;
-use Kdyby\QrEncode\DI\Configuration;
 
 
 
